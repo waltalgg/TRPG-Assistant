65be82946670613529605e0b7b7291425576bdae
waltal <waltalgg@yandex.ru>
2026-03-21 04:11:31 +0300

    fix: Исправления с библиотеками + vueRouter

--- frontend/App.vue
+++ frontend/App.vue
@@ -2,14 +2,18 @@
 </script>
 
 <template>
-  <header>
-    <link href="/src/assets/main.css" rel="stylesheet">
-  </header>
+  <html>
+    <head>
 
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <link href="/src/assets/main.css" rel="stylesheet" />
+
+    </head>
+
+    <body>
+
+    </body>
+  </html>
+
+
 </template>
-
-<style scoped>
-</style>
+<style scoped></style>
